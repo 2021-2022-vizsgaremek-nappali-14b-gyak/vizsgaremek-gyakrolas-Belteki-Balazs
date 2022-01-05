71e140beed0bcecc1478fce3f3cb7f28f46fd054
belteki-balazs <sz0belbal@vasvari.org>
2022-01-05 13:49:57 +0100

    Kész a feladatrész(10)

--- Desktop/src/VizsgaremekTests/Models/ProgramInfoTests.cs
+++ Desktop/src/VizsgaremekTests/Models/ProgramInfoTests.cs
@@ -25,5 +25,63 @@ namespace Vizsgaremek.Models.Tests
             // assert
             Assert.AreEqual(expected, actual, "Version is not 0.0.3.0");
         }
+
+        [TestMethod()]
+        public void ProgramInfoTestAuthors()
+        {
+            // arrange
+            ProgramInfo programInfo = new ProgramInfo();
+            string expected = new string("Bélteki Balázs");
+
+            // act
+            string actual = programInfo.Authors;
+
+            // assert
+            Assert.AreEqual(expected, actual, "The author is not this person!");
+        }
+
+        [TestMethod()]
+        public void ProgramInfoTestTitle()
+        {
+            // arrange
+            ProgramInfo programInfo = new ProgramInfo();
+            string expected = new string("Vizsgaremek");
+
+            // act
+            string actual = programInfo.Title;
+
+            // assert
+            Assert.AreEqual(expected, actual, "This is not the title!");
+        }
+
+        [TestMethod()]
+        public void ProgramInfoTestDescription()
+        {
+            // arrange
+            ProgramInfo programInfo = new ProgramInfo();
+            string expected = new string("Program verzió navigáció működik");
+
+            // act
+            string actual = programInfo.Description;
+
+            // assert
+            Assert.AreEqual(expected, actual, "This is not the description!");
+        }
+
+        [TestMethod()]
+        public void ProgramInfoTestCompany()
+        {
+            // arrange
+            ProgramInfo programInfo = new ProgramInfo();
+            string expected = new string("Vasvári");
+
+            // act
+            string actual = programInfo.Company;
+
+            // assert
+            Assert.AreEqual(expected, actual, "This is not the company!");
+        }
     }
+
+
 }
